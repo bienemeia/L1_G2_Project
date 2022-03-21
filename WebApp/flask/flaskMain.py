8a2a07ef4e88837e1b49efa9cc9ad04834420ada
“GrahamCBell” <GrahamCBell@cmail.carleton.ca>
2022-03-20 23:31:56 -0400

    Got a graph that work with flask and JS to work!

--- WebApp/flask/flaskMain.py
+++ WebApp/flask/flaskMain.py
@@ -1,31 +1,50 @@
-from flask import Flask, render_template
-#rom helper_functions import firebase, process
+from flask import Flask, Markup, render_template
 import time
+
 app = Flask(__name__)
+
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
 
 @app.route("/")
 def index():
-  names = "Meia, Graham and Boshen"
-  return render_template('index.html', names=names)
+	names = "Meia, Graham and Boshen"
+	return render_template('index.html', names=names)
   
 @app.route("/bees")
 def bees():
-  return render_template('bees.html')
+	line_labels=labels
+	line_values=values
+	return render_template('bees.html', max=17000, labels=line_labels, values=line_values)
 
 @app.route("/login")
 def login():
-  # Login logic here
-  return render_template('login.html')
+	# Login logic here
+	return render_template('login.html')
 
 @app.route("/video")
 def video():
-  # video logic here
-  return render_template('video.html')
+	# video logic here
+	return render_template('video.html')
 
 # @app.route("/background_test")
 # def test():
   # print("Hello")
   
 if __name__ == "__main__":
-  initializing_network()
-  app.run(debug=True, host='0.0.0.0')
+	initializing_network()
+	app.run(debug=True, host='0.0.0.0')
--- WebApp/mainDataProcessing.py
+++ WebApp/mainDataProcessing.py
@@ -13,7 +13,6 @@ def main():
 	"databaseURL":"https://testhive-2bca5-default-rtdb.firebaseio.com/",
 	"storageBucket":"testhive-2bca5.appspot.com"
 	}
-	
 	# Initialize Firebase DB
 	hive_firebase = pyrebase.initialize_app(meia_config)
 	hive_db = hive_firebase.database()
@@ -23,7 +22,7 @@ def main():
 		db = sqlite3.connect("hiveDB.db")
 		db.row_factory = sqlite3.Row
 		cursor=db.cursor()
-		
+				
 		date = firebase.getDate(hive_db, 1, now)
 		tempDict = firebase.getTemperature(hive_db, 1, now)
 		temperature = [tempDict['base'], tempDict['inside'], tempDict['outside']]
